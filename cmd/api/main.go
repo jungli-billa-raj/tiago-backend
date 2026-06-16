fec665bdc37e8e2f01f41e852d3b0b54199193c5
Raj <rajbhkseries@gmail.com>
2026-06-17 00:58:48 +0530

    database setup. db.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,11 +9,17 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8880"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("MAX_IDLE_TIME", "30m"),
+		},
 	}
 
 	store := store.NewStorage(nil)
 
-	app := application{
+	app := &application{
 		config: cfg,
 		store:  store,
 	}

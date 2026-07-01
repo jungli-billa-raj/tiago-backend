ad6c2d32986164f168f3f8b1549e79609081af41
Raj <rajbhkseries@gmail.com>
2026-07-01 07:19:49 +0530

    first migration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"tiago/internal/db"
 	"tiago/internal/env"
 	"tiago/internal/store"
 )
@@ -10,14 +11,28 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8880"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("MAX_IDLE_TIME", "30m"),
 		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+
+	log.Print("Database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
